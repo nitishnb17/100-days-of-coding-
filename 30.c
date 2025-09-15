2781d6fb8616f5e7736f225bdaf2b745400d094e
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-09-15 09:03:43 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+  int n,rev=0,rem;
+  printf("enter a number :\n");
+  scanf("%d", &d)
+    while(n!=0){
+    rem=n%10;
+    rev=rev*10+rem;
+    n=n\10;
+  }
+printf("reverse of a number is %d" , rev);  
+return 0;
+}
