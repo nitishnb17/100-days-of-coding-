d7531e2b726a4cbd34fa3d1b621a16dd4687558a
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-09-23 23:11:26 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("HCF/GCD of the two numbers is: %d\n", a);
+
+    return 0;
+}
