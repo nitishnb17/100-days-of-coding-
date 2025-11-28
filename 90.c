40ddbbadc4c3c28afe08a9743b440868ecece9c9
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-11-28 08:28:00 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    // Taking input
+    printf("Enter a string: ");
+    gets(str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;  // Convert lowercase to uppercase
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32;  // Convert uppercase to lowercase
+    }
+
+    printf("Toggled String: %s", str);
+    return 0;
+}
