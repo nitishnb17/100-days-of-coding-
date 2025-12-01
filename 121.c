a16e204a8c3c97f7f62743e2461c41afe85ea72a
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:15:05 +0530

    Create 121.c

---
+++ 121.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char name[100];
+    int age;
+
+    
+    fp = fopen("info.txt", "w");
+
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+  
+    printf("Enter your name: ");
+    scanf("%[^\n]", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    
+    fprintf(fp, "Name: %s\nAge: %d\n", name, age
+    fclose(fp);
+
+    printf("Data successfully saved to info.txt\n");
+
+    return 0;
+}
