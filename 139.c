e99d5f0ab03d4ca290bbbf9c171a0d29d034d850
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:26:51 +0530

    Create 139.c

---
+++ 139.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef enum {
+    A = 5,
+    B = 10,
+    C = 20
+} Demo;
+
+int main() {
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+
+    return 0;
+}
