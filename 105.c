c98a601988b9e3133ce3e309ecdc66731d116216
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:04:16 +0530

    Add majority element finder in 105.c
    Implement a program to find the majority element in an array.

---
+++ 105.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    int candidate = -1, count = 0;
+    for (int i = 0; i < n; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+            count = 1;
+        } else if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    int freq = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == candidate) freq++;
+    }
+
+    if (freq > n / 2)
+        printf("%d\n", candidate);
+    else
+        printf("-1\n");
+
+    return 0;
+}
