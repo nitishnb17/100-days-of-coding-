cedf94cd94792d99db63f503e0cd37da2cf4d875
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:29:10 +0530

    Create 144.c

---
+++ 144.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+
+void printStudent(struct Student s) {
+    printf("\n--- Student Details ---\n");
+    printf("Name: %s\n", s.name);
+    printf("Roll Number: %d\n", s.roll_no);
+    printf("Marks: %.2f\n", s.marks);
+}
+
+int main() {
+    struct Student st;
+
+  
+    printf("Enter name: ");
+    scanf("%s", st.name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &st.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &st.marks);
+
+    
+    printStudent(st);
+
+    return 0;
+}
