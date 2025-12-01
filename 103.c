55899ae7c06a63efa3619c9dfa9660fe66ae3498
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:03:16 +0530

    Implement pivot index finder in C
    This program finds the pivot index in an array where the sum of elements on the left equals the sum of elements on the right.

---
+++ 103.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int totalSum = 0;
+    for (int i = 0; i < n; i++) {
+        totalSum += arr[i];
+    }
+
+    int leftSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int rightSum = totalSum - leftSum - arr[i];
+
+        if (leftSum == rightSum) {
+            printf("Pivot index: %d\n", i);
+            return 0;   
+        }
+
+        leftSum += arr[i];
+    }
+
+    printf("Pivot index: -1\n");
+    return 0;
+}
