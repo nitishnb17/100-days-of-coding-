29f9fc3eeee8dceb23890592f54c1b6991c6327d
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:12:42 +0530

    Create 116.c

---
+++ 116.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d positive integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                printf("%d %d\n", i, j);
+                return 0;
+            }
+        }
+    }
+
+    printf("-1 -1\n");
+    return 0;
+}
