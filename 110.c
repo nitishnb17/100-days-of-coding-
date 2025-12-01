cedba2c5c554fc5de2ee4817f5d52e01639066fb
Nitish Bhardwaj <nitishbhardwaj170907@gmail.com>
2025-12-01 11:08:50 +0530

    Add program to find max in subarrays of size k
    Implement a program to find the maximum of each subarray of size k.

---
+++ 110.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("-1\n");
+        return 0;
+    }
+
+    
+    for (int i = 0; i <= n - k; i++) {
+        int max = arr[i];
+
+        for (int j = i; j < i + k; j++) {
+            if (arr[j] > max)
+                max = arr[j];
+        }
+
+        printf("%d ", max);
+    }
+
+    printf("\n");
+    return 0;
+}
